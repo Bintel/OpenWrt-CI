4dc3fafcff958d97af08da4a8474e636fa46d663
Bintel <i@bintel.me>
2023-07-07 23:37:15 +0800

    Add package luci-app-ddns-go

--- compile.sh
+++ compile.sh
@@ -27,6 +27,7 @@ Prepare()
     cd ./openwrt
     time Customize
     echo "src-git helloworld https://github.com/fw876/helloworld" >> ./feeds.conf.default
+    echo "src-git ddnsgo https://github.com/sirpdboy/luci-app-ddns-go" >> ./feeds.conf.default
     ./scripts/feeds update -a
     ./scripts/feeds install -a
     cd ..
