67e20d30e3afc36e87a6db2848ad612b6c76d2b4
Bintel <i@bintel.me>
2024-01-31 12:43:44 +0800

    Remove Customize

--- compile.sh
+++ compile.sh
@@ -25,19 +25,12 @@ Prepare()
     PrintMessage "Preparing the source code...\n"
     git clone --depth 1 https://github.com/coolsnowwolf/lede openwrt
     cd ./openwrt
-    time Customize
     echo "src-git helloworld https://github.com/fw876/helloworld;main" >> ./feeds.conf.default
     echo "src-git ddnsgo https://github.com/sirpdboy/luci-app-ddns-go" >> ./feeds.conf.default
     ./scripts/feeds update -a
     ./scripts/feeds install -a
     cd ..
     PrintMessage "Preparing the source code is done. Time used: "
-}
-Customize()
-{
-    PrintMessage "Customizing the source code...\n"
-    sed -i 's/define Device\/zte_mf269/define Device\/redmi_ax6\n\t$(call Device\/xiaomi_ax3600)\n\tDEVICE_VENDOR := Redmi\n\tDEVICE_MODEL := AX6\n\tDEVICE_PACKAGES := ipq-wifi-redmi_ax6 uboot-envtools\nendef\nTARGET_DEVICES += redmi_ax6\n\ndefine Device\/xiaomi_ax3600\n\t$(call Device\/FitImage)\n\t$(call Device\/UbiFit)\n\tDEVICE_VENDOR := Xiaomi\n\tDEVICE_MODEL := AX3600\n\tBLOCKSIZE := 128k\n\tPAGESIZE := 2048\n\tDEVICE_DTS_CONFIG := config@ac04\n\tSOC := ipq8071\n\tDEVICE_PACKAGES := ath10k-firmware-qca9887-ct ipq-wifi-xiaomi_ax3600 \\\n\tkmod-ath10k-ct uboot-envtools\nendef\nTARGET_DEVICES += xiaomi_ax3600\n\ndefine Device\/zte_mf269/' ./target/linux/ipq807x/image/generic.mk
-    PrintMessage "Customizing the source code is done. Time used: "
 }
 Compile()
 {
