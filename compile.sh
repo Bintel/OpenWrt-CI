506ef3538dff0aebcfc873c5a1f2ba3af61b6683
Bintel <i@bintel.me>
2026-03-03 23:33:55 +0800

    Initial commit

---
+++ compile.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+TIMEFORMAT="%E"
+BASEPATH=$(dirname `realpath $BASH_SOURCE`)
+CURRENTPATH=`pwd`
+Main()
+{
+    if [ $# -eq 0 ]
+    then
+        PrintMessage "Please enter target as argument.\n"
+        exit 1
+    fi
+    cd $BASEPATH
+    rm -rf ./openwrt/
+    df -h
+    time Prepare
+    for arg in $@
+    do
+        time Compile $arg
+    done
+    df -h
+    cd $CURRENTPATH
+}
+Prepare()
+{
+    PrintMessage "Preparing the source code...\n"
+    git clone --depth 1 https://github.com/coolsnowwolf/lede openwrt
+    cd ./openwrt
+    sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
+    sed -i '/src-git luci/d' ./feeds.conf.default
+    echo "src-git luci https://github.com/coolsnowwolf/luci" >> ./feeds.conf.default
+    echo "src-git ddnsgo https://github.com/sirpdboy/luci-app-ddns-go" >> ./feeds.conf.default
+    cat ./feeds.conf.default
+    ./scripts/feeds update -a
+    ./scripts/feeds install -a
+    cd ..
+    PrintMessage "Preparing the source code is done. Time used: "
+}
+Compile()
+{
+    local platform=$1
+    PrintMessage "Compiling the firmware for $platform...\n"
+    cd ./openwrt
+    cat ../luci-app.config ../targets/$platform.config > ./.config
+    make defconfig
+    cat ./.config
+    make download -j$(nproc) || make download -j1 V=s
+    make -j$(nproc) || make -j1 V=s
+    mv ./.config ./$platform.config
+    cd ..
+    PrintMessage "Compiling the firmware for $platform is done. Time used: "
+}
+PrintMessage()
+{
+    local message=$1
+    echo -n -e "\033[0;34m$message\033[0m"
+}
+Main $@
